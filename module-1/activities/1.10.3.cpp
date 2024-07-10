1a80f86753b5c6e3cd36a14a4326aaa0821e080e
Angel Davila <adavila0703@gmail.com>
2024-07-10 16:31:35 -0500

    1.10.3

--- module-1/activities/1.10.3.cpp
+++ module-1/activities/1.10.3.cpp
@@ -1,12 +1,11 @@
 /*
-1.10.2: Copy and modify vector elements.
-Write a loop that sets newScores to oldScores shifted once left, with element 0 copied to the end.
-Ex: If oldScores = {10, 20, 30, 40}, then newScores = {20, 30, 40, 10}.
+1.10.3: Modify vector elements using other elements.
+Write a loop that sets each vector element to the sum of itself and the next element,
+except for the last element which stays the same. Be careful not to index beyond the last element. Ex:
 
-Note: These activities may test code with different test values. This activity will perform two tests, both with a 4-element array. See "How to Use zyBooks".
-
-Also note: If the submitted code tries to access an invalid array element, such as newScores[9] for a 4-element array, the test may generate strange results.
-Or the test may crash and report "Program end never reached", in which case the system doesn't print the test case that caused the reported message.
+Initial scores:        10, 20, 30, 40
+Scores after the loop: 30, 50, 70, 40
+The first element is 30 or 10 + 20, the second element is 50 or 20 + 30, and the third element is 70 or 30 + 40. The last element remains the same.
 */
 
 #include <iostream>
@@ -16,32 +15,30 @@ using namespace std;
 int main()
 {
 	const int SCORES_SIZE = 4;
-	vector<int> oldScores(SCORES_SIZE);
-	vector<int> newScores(SCORES_SIZE);
+	vector<int> bonusScores(SCORES_SIZE);
 	unsigned int i;
 
-	for (i = 0; i < oldScores.size(); ++i)
+	for (i = 0; i < bonusScores.size(); ++i)
 	{
-		cin >> oldScores.at(i);
+		cin >> bonusScores.at(i);
 	}
 
 	/* Your solution goes here  */
-	int firstElement = oldScores.at(0);
-	int lastIndex = oldScores.size() - 1;
-	for (i = 0; i < oldScores.size(); i++)
+	int lastIndex = bonusScores.size() - 1;
+
+	for (i = 0; i < bonusScores.size(); i++)
 	{
 		if (i == lastIndex)
 		{
-			newScores.at(i) = firstElement;
 			break;
 		}
 
-		newScores.at(i) = oldScores.at(i + 1);
+		bonusScores.at(i) += bonusScores.at(i + 1);
 	}
 
-	for (i = 0; i < newScores.size(); ++i)
+	for (i = 0; i < bonusScores.size(); ++i)
 	{
-		cout << newScores.at(i) << " ";
+		cout << bonusScores.at(i) << " ";
 	}
 	cout << endl;
 
